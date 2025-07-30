20a2a405efe24813564a2da2d6d52bb5ec39357d
MeNameAri <ariproject20@gmail.com>
2025-07-31 03:33:46 +0700

    Hazard & Interactive Mechanic
    Add Program for Hazard and Interactive Mechanics such as Stun and Knockback Things and Trampoline for interactive

---
+++ Assets/Ari`s Script Workshop/Player/TrampoulineObject.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampoulineObject : MonoBehaviour
+{private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Cek apakah yang masuk trigger adalah player
+        if (other.CompareTag("Players"))
+        {
+            Rigidbody2D playerRb = other.GetComponent<Rigidbody2D>();
+
+            // Pastikan player sedang jatuh (agar tidak terpantul saat berjalan di atasnya)
+            if (playerRb != null && playerRb.velocity.y <= 0f)
+            {
+                PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
+                if (playerMovement != null)
+                {
+                    // Panggil fungsi pantulan di player
+                    playerMovement.BounceOnTrampoline();
+                }
+            }
+        }
+    }
+}
